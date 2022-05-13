405161cba79c37238f25a93c35b8c081034ea10e
sudipghimire533 <sudipghimire533@gmail.com>
2022-05-13 03:33:51 +0000

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
 SCRIPTDIR=$PWD
 for d in $(ls -d ./pallets/*/) ; do
-    cd "$SCRIPTDIR/$d" && cargo test
+    PALLET_DIR="$SCRIPTDIR/$d"
+    cd $PALLET_DIR
+    
+    TEST_FILE="$PALLET_DIR/./test.sh"
+    if [ -f "$TEST_FILE" ]; then
+        bash $TEST_FILE
+    else
+        cargo test
+    fi
+
 done
