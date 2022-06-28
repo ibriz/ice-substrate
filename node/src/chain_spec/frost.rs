48d23fa9b4e2a82572ca218b59e5e5e36bc82cb1
sudipghimire533 <sudipghimire533@gmail.com>
2022-06-28 06:38:07 +0000

    Add exchange accounts in frost genesis

--- node/src/chain_spec/frost.rs
+++ node/src/chain_spec/frost.rs
@@ -1,14 +1,15 @@
 use frost_runtime::{
-	AccountId, AuraConfig, BalancesConfig,AirdropConfig, CouncilConfig, EVMConfig, EthereumConfig, GenesisConfig, GrandpaConfig,
-	Signature, SudoConfig, SystemConfig, WASM_BINARY, currency::ICY, SessionConfig, opaque::SessionKeys, PalletId
+	currency::ICY, opaque::SessionKeys, AccountId, AirdropConfig, AuraConfig, BalancesConfig,
+	CouncilConfig, EVMConfig, EthereumConfig, GenesisConfig, GrandpaConfig, PalletId,
+	SessionConfig, Signature, SudoConfig, SystemConfig, WASM_BINARY,
 };
+use hex_literal::hex;
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{crypto::UncheckedInto, sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
-use sp_runtime::traits::{IdentifyAccount, Verify, AccountIdConversion};
-use std::{collections::BTreeMap};
-use hex_literal::hex;
+use sp_runtime::traits::{AccountIdConversion, IdentifyAccount, Verify};
+use std::collections::BTreeMap;
 use std::marker::PhantomData;
 
 // The URL for the telemetry server.
@@ -48,6 +49,29 @@ pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
 // Treasury Pallet ID
 const TREASURY_PALLET_ID: PalletId = PalletId(*b"py/trsry");
 
+const AIRDROP_EXCHANGE_ACCOUNTS: &[([u8; 20], u128)] = &[
+	(
+		hex_literal::hex!("562dc1e2c7897432c298115bc7fbcc3b9d5df294"),
+		70717613544517522852341727,
+	),
+	(
+		hex_literal::hex!("61acc986a761b5f354dc8777360aeaf47b2ab616"),
+		8968750000000000000,
+	),
+	(
+		hex_literal::hex!("6d14b2b77a9e73c5d5804d43c7e3c3416648ae3d"),
+		8348890436199324029817984,
+	),
+	(
+		hex_literal::hex!("938b9a413de9ffbbeae72e7034931a3bdf0f1e96"),
+		2959971579000000000000000 + 13972742011003245391080,
+	),
+	(
+		hex_literal::hex!("d182113fea7ae3164871bfda90ec8652123aa354"),
+		352948797792142357220773,
+	),
+];
+
 /// Initialize frost testnet configuration
 pub fn testnet_config() -> Result<FrostChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
@@ -65,25 +89,31 @@ pub fn testnet_config() -> Result<FrostChainSpec, String> {
 				vec![
 					(
 						// AuraId
-						hex!["62687296bffd79f12178c4278b9439d5eeb8ed7cc0b1f2ae29307e806a019659"].unchecked_into(),
+						hex!["62687296bffd79f12178c4278b9439d5eeb8ed7cc0b1f2ae29307e806a019659"]
+							.unchecked_into(),
 						// GrandpaId
-						hex!["27c6da25d03bb6b3c751da3e8c5265b0bb357c15240602443cc286c0658b47f9"].unchecked_into(),
+						hex!["27c6da25d03bb6b3c751da3e8c5265b0bb357c15240602443cc286c0658b47f9"]
+							.unchecked_into(),
 					),
 					(
-						hex!["d893ef775b5689473b2e9fa32c1f15c72a7c4c86f05f03ee32b8aca6ce61b92c"].unchecked_into(),
-						hex!["85ec524aeacb6e558619a10da82cdf787026209211d1b7462cb176d58f2add86"].unchecked_into()
-					)
+						hex!["d893ef775b5689473b2e9fa32c1f15c72a7c4c86f05f03ee32b8aca6ce61b92c"]
+							.unchecked_into(),
+						hex!["85ec524aeacb6e558619a10da82cdf787026209211d1b7462cb176d58f2add86"]
+							.unchecked_into(),
+					),
 				],
 				// Council members
-                vec![
-                    hex!["62687296bffd79f12178c4278b9439d5eeb8ed7cc0b1f2ae29307e806a019659"].into()
-                ],
+				vec![
+					hex!["62687296bffd79f12178c4278b9439d5eeb8ed7cc0b1f2ae29307e806a019659"].into(),
+				],
 				// Sudo account
 				hex!["62687296bffd79f12178c4278b9439d5eeb8ed7cc0b1f2ae29307e806a019659"].into(),
 				// Creditor account
 				hex!("10b3ae7ebb7d722c8e8d0d6bf421f6d5dbde8d329f7c905a201539c635d61872").into(),
 				// Airdrop merkle root
 				[0u8; 32],
+				// Airdrop exchange account
+				AIRDROP_EXCHANGE_ACCOUNTS.to_vec(),
 				// Pre-funded accounts
 				vec![
 					TREASURY_PALLET_ID.into_account(),
@@ -122,16 +152,16 @@ pub fn development_config() -> Result<FrostChainSpec, String> {
 				wasm_binary,
 				// Initial PoA authorities
 				vec![authority_keys_from_seed("Alice")],
-                // Council members
-				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice")
-				],
+				// Council members
+				vec![get_account_id_from_seed::<sr25519::Public>("Alice")],
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Creditor account
 				hex!("10b3ae7ebb7d722c8e8d0d6bf421f6d5dbde8d329f7c905a201539c635d61872").into(),
 				// Airdrop merkle root
 				hex!("4c59b428da385567a6d42ee1881ecbe43cf30bf8c4499887b7c6f689d23d4672").into(),
+				// Airdrop exchange accounts pair
+				AIRDROP_EXCHANGE_ACCOUNTS.to_vec(),
 				// Pre-funded accounts
 				vec![
 					TREASURY_PALLET_ID.into_account(),
@@ -176,15 +206,15 @@ pub fn local_testnet_config() -> Result<FrostChainSpec, String> {
 					authority_keys_from_seed("Bob"),
 				],
 				// Council members
-                vec![
-                    get_account_id_from_seed::<sr25519::Public>("Alice")
-                ],
+				vec![get_account_id_from_seed::<sr25519::Public>("Alice")],
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Creditor account,
 				hex!("10b3ae7ebb7d722c8e8d0d6bf421f6d5dbde8d329f7c905a201539c635d61872").into(),
 				// Airdrop merkle root
 				hex!("4c59b428da385567a6d42ee1881ecbe43cf30bf8c4499887b7c6f689d23d4672").into(),
+				// Exhcnage accounts for Airdrop
+				AIRDROP_EXCHANGE_ACCOUNTS.to_vec(),
 				// Pre-funded accounts
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
@@ -219,7 +249,7 @@ pub fn local_testnet_config() -> Result<FrostChainSpec, String> {
 
 /// Helper for session keys to map aura id
 fn session_keys(aura: AuraId, grandpa: GrandpaId) -> SessionKeys {
-    SessionKeys {aura, grandpa}
+	SessionKeys { aura, grandpa }
 }
 
 /// Configure initial storage state for FRAME modules.
@@ -230,21 +260,22 @@ fn testnet_genesis(
 	root_key: AccountId,
 	creditor_account: AccountId,
 	airdrop_merkle_root: [u8; 32],
+	airdrop_exchange_accounts: Vec<([u8; 20], u128)>,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
 ) -> GenesisConfig {
 	let authorities = vec![
-        (
-            get_account_id_from_seed::<sr25519::Public>("Alice"),
-            authority_keys_from_seed("Alice").0,
+		(
+			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			authority_keys_from_seed("Alice").0,
 			authority_keys_from_seed("Alice").1,
-        ),
-        (
-            get_account_id_from_seed::<sr25519::Public>("Bob"),
+		),
+		(
+			get_account_id_from_seed::<sr25519::Public>("Bob"),
 			authority_keys_from_seed("Bob").0,
 			authority_keys_from_seed("Bob").1,
-        ),
-    ];
+		),
+	];
 	GenesisConfig {
 		system: SystemConfig {
 			// Add Wasm runtime to storage.
@@ -255,7 +286,7 @@ fn testnet_genesis(
 				.iter()
 				.cloned()
 				.map(|k| (k, ICY * 40_000))
-				.collect()
+				.collect(),
 		},
 		aura: AuraConfig {
 			authorities: vec![],
@@ -269,10 +300,16 @@ fn testnet_genesis(
 		},
 		session: SessionConfig {
 			keys: authorities
-			.iter()
-			.map(|x| (x.0.clone(), x.0.clone(), session_keys(x.1.clone(), x.2.clone())))
-			.collect::<Vec<_>>(),
-	      },
+				.iter()
+				.map(|x| {
+					(
+						x.0.clone(),
+						x.0.clone(),
+						session_keys(x.1.clone(), x.2.clone()),
+					)
+				})
+				.collect::<Vec<_>>(),
+		},
 		evm: EVMConfig {
 			accounts: {
 				let map = BTreeMap::new();
@@ -282,17 +319,17 @@ fn testnet_genesis(
 		ethereum: EthereumConfig {},
 		dynamic_fee: Default::default(),
 		base_fee: Default::default(),
-		vesting:Default::default(),
+		vesting: Default::default(),
 		assets: Default::default(),
-        council: CouncilConfig {
-            members: council_members,
-            phantom: PhantomData,
-        },
-        treasury: Default::default(),
+		council: CouncilConfig {
+			members: council_members,
+			phantom: PhantomData,
+		},
+		treasury: Default::default(),
 		airdrop: AirdropConfig {
 			creditor_account: creditor_account,
 			merkle_root: airdrop_merkle_root,
-			exchange_accounts: vec![],
+			exchange_accounts: airdrop_exchange_accounts,
 		},
 	}
 }
