a3b366cea275186e82750af663f289cce67288b3
sabin <sabinchitrakar11@gmail.com>
2022-11-07 08:37:30 +0545

    integrate dispatch precompile

--- runtime/frost/src/precompile.rs
+++ runtime/frost/src/precompile.rs
@@ -4,7 +4,14 @@ use sp_std::marker::PhantomData;
 
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
+use pallet_evm_precompile_dispatch::Dispatch;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+use frame_support::{
+	codec::{Decode, DecodeLimit as _},
+	dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo},
+	traits::{ConstU32, Get},
+	weights::{DispatchClass, Pays},
+};
 
 pub struct FrontierPrecompiles<R>(PhantomData<R>);
 
@@ -17,7 +24,7 @@ where
 	}
 
 	pub fn used_addresses() -> sp_std::vec::Vec<H160> {
-		sp_std::vec![1, 2, 3, 4, 5, 1024, 1025]
+		sp_std::vec![1, 2, 3, 4, 5, 6,1024, 1025]
 			.into_iter()
 			.map(hash)
 			.collect()
@@ -33,9 +40,11 @@ where
 	}
 }
 
-impl<R> PrecompileSet for FrontierPrecompiles<R>
+impl<T> PrecompileSet for FrontierPrecompiles<T>
 where
-	R: pallet_evm::Config,
+	T: pallet_evm::Config,
+	T::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo + Decode,
+	<T::Call as Dispatchable>::Origin: From<Option<T::AccountId>>,
 {
 	fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult> {
 		match handle.code_address() {
@@ -45,6 +54,7 @@ where
 			a if a == hash(3) => Some(Ripemd160::execute(handle)),
 			a if a == hash(4) => Some(Identity::execute(handle)),
 			a if a == hash(5) => Some(Modexp::execute(handle)),
+			a if a == hash(6) => Some(Dispatch::<T>::execute(handle)),
 			// Non-Frontier specific nor Ethereum precompiles :
 			a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
 			a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
